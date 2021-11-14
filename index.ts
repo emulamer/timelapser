c15869f18646fc65a90643b3c71d4e41d11cce3f
Max Myers <max@emulamer.com>
2021-11-14 09:28:02 -0500

    do quick timelapse ffmpeg thing

---
+++ index.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import {execSync} from 'child_process';
+
+const IMG_PATH = "/home/pi/phogrows";
+const TMP_PATH = path.join(__dirname, ".tmp");
+if (!fs.existsSync(TMP_PATH)) {
+    fs.mkdirSync(TMP_PATH);
+}
+const files = fs.readdirSync(IMG_PATH).filter(x=> x.startsWith("auto_") && x.endsWith(".png")).sort();
+const cleanupFiles = [];
+try {
+    let ctr = 0;
+    for (let f of files) {
+        const tmpFile = path.join(TMP_PATH, `img${ctr}.png`);
+        fs.linkSync(path.join(IMG_PATH, f), tmpFile);
+        cleanupFiles.push(tmpFile);
+        ctr++;
+    }
+    console.log(execSync(`${path.join(__dirname, 'ffmpeg_win', "bin", "ffmpeg.exe")} -r 1 -f image2 -s 1920x1080 -i img%d.png -vcodec libx264 -crf 15 -pix_fmt yuv420p out.mp4`, { cwd: TMP_PATH }));
+    execSync(`mv '${path.join(TMP_PATH, "out.mp4")}' /home/pi/phogrows/timelapse.mp4`);
+} finally {
+    for (let f of cleanupFiles) {
+        console.log("unlinking " + f);
+        fs.unlinkSync(f);
+    }
+}
+
+
